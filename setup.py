a339bb4f7c34598afbba1f64573282ed00e1b9a5
rusty1s <matthias.fey@tu-dortmund.de>
2018-07-28 20:53:20 +0200

    unused imports

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import torch
 from setuptools import setup, find_packages
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 __version__ = '1.0.1'
 url = 'https://github.com/rusty1s/pytorch_sparse'
@@ -17,7 +16,8 @@ if torch.cuda.is_available():
 setup(
     name='torch_sparse',
     version=__version__,
-    description='PyTorch Extension Library of Optimized Sparse Operations',
+    description='PyTorch Extension Library of Optimized Sparse Matrix '
+    'Operations',
     author='Matthias Fey',
     author_email='matthias.fey@tu-dortmund.de',
     url=url,
