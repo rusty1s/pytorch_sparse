70249ef73c91627ac4502218f6c05191a966e3e2
rusty1s <matthias.fey@tu-dortmund.de>
2018-08-05 10:52:43 +0200

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import torch
 from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 url = 'https://github.com/rusty1s/pytorch_sparse'
 
 install_requires = ['numpy', 'scipy']
--- torch_sparse/__init__.py
+++ torch_sparse/__init__.py
@@ -2,7 +2,10 @@ from .coalesce import coalesce
 from .transpose import transpose
 from .matmul import spspmm
 
+__version__ = '0.2.0'
+
 __all__ = [
+    '__version__',
     'coalesce',
     'transpose',
     'spspmm',
