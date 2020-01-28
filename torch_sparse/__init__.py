f6bf81dfb65f1e281789cd2b321fcd924419b2d6
rusty1s <matthias.fey@tu-dortmund.de>
2020-01-28 17:32:14 +0100

    move imports

--- torch_sparse/__init__.py
+++ torch_sparse/__init__.py
@@ -1,3 +1,18 @@
+import torch
+
+torch.ops.load_library('torch_sparse/convert_cpu.so')
+torch.ops.load_library('torch_sparse/diag_cpu.so')
+torch.ops.load_library('torch_sparse/spmm_cpu.so')
+
+try:
+    torch.ops.load_library('torch_sparse/convert_cuda.so')
+    torch.ops.load_library('torch_sparse/diag_cuda.so')
+    torch.ops.load_library('torch_sparse/spmm_cuda.so')
+    torch.ops.load_library('torch_sparse/spspmm_cuda.so')
+except OSError as e:
+    if torch.cuda.is_available():
+        raise e
+
 from .convert import to_torch_sparse, from_torch_sparse, to_scipy, from_scipy
 from .coalesce import coalesce
 from .transpose import transpose
--- torch_sparse/utils.py
+++ torch_sparse/utils.py
@@ -1,24 +1,9 @@
 from typing import Any
 
-import torch
-
 try:
     from typing_extensions import Final  # noqa
 except ImportError:
     from torch.jit import Final  # noqa
-
-torch.ops.load_library('torch_sparse/convert_cpu.so')
-torch.ops.load_library('torch_sparse/diag_cpu.so')
-torch.ops.load_library('torch_sparse/spmm_cpu.so')
-
-try:
-    torch.ops.load_library('torch_sparse/convert_cuda.so')
-    torch.ops.load_library('torch_sparse/diag_cuda.so')
-    torch.ops.load_library('torch_sparse/spmm_cuda.so')
-    torch.ops.load_library('torch_sparse/spspmm_cuda.so')
-except OSError as e:
-    if torch.cuda.is_available():
-        raise e
 
 
 def is_scalar(other: Any) -> bool:
