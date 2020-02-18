4903fcdeca84b96931ef2877e6ed7ff938c9ff72
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-17 20:41:29 -0800

    error handling

--- torch_sparse/__init__.py
+++ torch_sparse/__init__.py
@@ -11,13 +11,12 @@ try:
         torch.ops.load_library(importlib.machinery.PathFinder().find_spec(
             library, [osp.dirname(__file__)]).origin)
 except OSError as e:
-    if 'undefined symbol' in str(e):
-        major, minor = [int(x) for x in torch.__version__.split('.')[:2]]
-        t_major, t_minor = expected_torch_version
-        if major != t_major or (major == t_major and minor != t_minor):
-            raise RuntimeError(
-                f'Expected PyTorch version {t_major}.{t_minor} but found '
-                f'version {major}.{minor}.')
+    major, minor = [int(x) for x in torch.__version__.split('.')[:2]]
+    t_major, t_minor = expected_torch_version
+    if major != t_major or (major == t_major and minor != t_minor):
+        raise RuntimeError(
+            f'Expected PyTorch version {t_major}.{t_minor} but found '
+            f'version {major}.{minor}.')
     raise OSError(e)
 
 if torch.version.cuda is not None:  # pragma: no cover
