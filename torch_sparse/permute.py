af9127e37849af0281ee1e142cf97cd24693411c
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-19 14:45:06 -0800

    fix permute

--- torch_sparse/permute.py
+++ torch_sparse/permute.py
@@ -1,32 +1,10 @@
 import torch
-from torch_sparse.storage import SparseStorage
 from torch_sparse.tensor import SparseTensor
 
 
 def permute(src: SparseTensor, perm: torch.Tensor) -> SparseTensor:
-    assert src.is_symmetric()
-
-    row, col, value = src.coo()
-
-    row = perm[row]
-    col = perm[col]
-
-    if value is not None:
-        value = value[row]
-
-    rowcount = src.storage._rowcount
-    if rowcount is not None:
-        rowcount = rowcount[perm]
-
-    colcount = src.storage._colcount
-    if colcount is not None:
-        colcount = colcount[perm]
-
-    storage = SparseStorage(row=row, rowptr=None, col=col, value=value,
-                            sparse_sizes=src.sparse_sizes(), rowcount=rowcount,
-                            colptr=None, colcount=colcount, csr2csc=None,
-                            csc2csr=None, is_sorted=False)
-    return src.from_storage(storage)
+    assert src.is_quadratic()
+    return src.index_select(0, perm).index_select(1, perm)
 
 
 SparseTensor.permute = lambda self, perm: permute(self, perm)
