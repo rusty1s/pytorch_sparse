8e01fb7caea00fec00ce3f404a9290fdfa0f0a26
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-20 10:30:38 -0800

    typo

--- test/test_metis.py
+++ test/test_metis.py
@@ -10,4 +10,4 @@ def test_metis(device):
     mat = SparseTensor.from_dense(torch.randn((6, 6), device=device))
     mat, partptr, perm = mat.partition_kway(num_parts=2)
     assert partptr.numel() == 3
-    assert perm.nume() == 6
+    assert perm.numel() == 6
