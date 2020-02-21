4df567086a53e58486c0912da7809334a598edd5
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-21 08:43:57 -0800

    windows

--- script/metis.sh
+++ script/metis.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-if [ "${TRAVIS_OS_NAME}" = "windows" ]; then
-  choco install make
-  cmake --help
-fi
-
 METIS=metis-5.1.0
 
 wget -nv http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/${METIS}.tar.gz
 tar -xvzf ${METIS}.tar.gz
 cd ${METIS} || exit
 sed -i.bak -e 's/IDXTYPEWIDTH 32/IDXTYPEWIDTH 64/g' include/metis.h
-echo "CONFIG"
-make config
-echo "MAKE"
-make
-echo "MAKE INSTALL"
-sudo make install
+
+if [ "${TRAVIS_OS_NAME}" = "windows" ]; then
+  make config
+  make
+  sudo make install
+else
+  ./vsgen
+fi
 cd ..
