a75377b8d22a57cb734a756f9d2f3a9afa313c35
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-21 07:47:22 -0800

    echo

--- script/metis.sh
+++ script/metis.sh
@@ -10,7 +10,10 @@ wget -nv http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/${METIS}.tar.gz
 tar -xvzf ${METIS}.tar.gz
 cd ${METIS} || exit
 sed -i.bak -e 's/IDXTYPEWIDTH 32/IDXTYPEWIDTH 64/g' include/metis.h
+echo "CONFIG"
 make config
+echo "MAKE"
 make
+echo "MAKE INSTALL"
 sudo make install
 cd ..
