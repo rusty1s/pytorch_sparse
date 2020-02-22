1cf814fe8e683bc2bff9a9ba3c25458783e71d75
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-22 10:50:53 -0800

    copy library

--- script/metis.sh
+++ script/metis.sh
@@ -15,22 +15,13 @@ else
   # Fix GKlib on Windows: https://github.com/jlblancoc/suitesparse-metis-for-windows/issues/6
   sed -i.bak -e '61,69d' GKlib/gk_arch.h
 
-  mkdir build
   cd build || exit
 
   cmake ..
   cmake --build . --config "Release" --target ALL_BUILD
 
-  echo "--- LIBMETIS"
-  ls libmetis
-  echo "--- RELEASE"
-  ls libmetis/Release
-
-  echo "$LIBRARY_LIB"
-  echo "$LIBRARY_INC"
-  ls "/c/tools/miniconda3/envs/test"
-  ls "/c/tools/miniconda3/envs/libs"
-  ls "/c/tools/miniconda3/envs/include"
+  cp libmetis/Release/metis.lib /c/tools/miniconda3/envs/libs
+  cp ../include/metis.h /c/tools/miniconda3/envs/include
 
   cd ..
 fi
