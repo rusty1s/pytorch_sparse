a597b8229f9c38568dde99b9755372ea3d66f572
rusty1s <matthias.fey@tu-dortmund.de>
2020-03-21 13:01:40 +0100

    only cpu test

--- test/test_saint.py
+++ test/test_saint.py
@@ -1,15 +1,11 @@
-import pytest
 import torch
 from torch_sparse.tensor import SparseTensor
 
-from .utils import devices
 
-
-@pytest.mark.parametrize('device', devices)
-def test_saint_subgraph(device):
+def test_saint_subgraph():
     row = torch.tensor([0, 0, 1, 1, 2, 2, 2, 3, 3, 4])
     col = torch.tensor([1, 2, 0, 2, 0, 1, 3, 2, 4, 3])
-    adj = SparseTensor(row=row, col=col).to(device)
+    adj = SparseTensor(row=row, col=col)
     node_idx = torch.tensor([0, 1, 2])
 
     adj, edge_index = adj.saint_subgraph(node_idx)
