6e57f2051554a0b3a2790b238470c72ab5454a6f
rusty1s <matthias.fey@tu-dortmund.de>
2020-04-25 07:03:23 +0200

    [ci deploy]

--- script/install.sh
+++ script/install.sh
@@ -4,6 +4,7 @@ if [ "${TRAVIS_OS_NAME}" != "windows" ]; then
   python setup.py install
 else
   echo "python setup.py install"
-  while sleep 9m; do echo "=====[ $SECONDS seconds still running ]====="; done &
-  python setup.py install &> /dev/null
+  while sleep 540; do echo "=====[ Still running after $SECONDS seconds ]====="; done &
+  python setup.py install | tail -n 1000
+  kill %1
 fi
