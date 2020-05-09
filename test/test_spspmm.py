2317ff66a66688246b12dee9d3d560e2bc930875
Mario Geiger <geiger.mario@gmail.com>
2020-05-09 20:47:23 +0200

    test_spspmm_2

--- test/test_spspmm.py
+++ test/test_spspmm.py
@@ -2,7 +2,7 @@ from itertools import product
 
 import pytest
 import torch
-from torch_sparse import spspmm, SparseTensor
+from torch_sparse import spspmm, SparseTensor, transpose
 
 from .utils import grad_dtypes, devices, tensor
 
@@ -17,6 +17,38 @@ def test_spspmm(dtype, device):
     indexC, valueC = spspmm(indexA, valueA, indexB, valueB, 3, 3, 2)
     assert indexC.tolist() == [[0, 1, 2], [0, 1, 1]]
     assert valueC.tolist() == [8, 6, 8]
+
+
+@pytest.mark.parametrize('dtype,device', product(grad_dtypes, devices))
+def test_spspmm_2(dtype, device):
+    row = torch.tensor(
+        [0, 1, 1, 1, 2, 3, 4, 5, 5, 6, 6, 7, 7, 7, 8, 8, 9, 9],
+        device=device
+    )
+    col = torch.tensor(
+        [0, 5, 10, 15, 1, 2, 3, 7, 13, 6, 9, 5, 10, 15, 11, 14, 5, 15],
+        device=device
+    )
+    value = torch.tensor(
+        [1, 3**-0.5, 3**-0.5, 3**-0.5, 1, 1, 1, -2**-0.5, -2**-0.5,
+         -2**-0.5, -2**-0.5, 6**-0.5, -6**0.5 / 3, 6**-0.5, -2**-0.5,
+         -2**-0.5, 2**-0.5, -2**-0.5],
+        dtype=dtype, device=device
+    )
+    index = torch.stack([row, col])
+
+    m = value.new_zeros(10, 16)
+    m[index[0], index[1]] = value
+
+    index_t, value_t = transpose(index, value, 10, 16)
+
+    index, value = spspmm(index, value, index_t, value_t, 10, 16, 10)
+
+    mask = value.abs() > 1e-4
+    index, value = index[:, mask], value[mask]
+
+    assert index.tolist() == [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]
+    assert value.tolist() == [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 
 @pytest.mark.parametrize('dtype,device', product(grad_dtypes, devices))
