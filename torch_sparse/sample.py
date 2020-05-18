fix broadcasting

torch_sparse/sample.py
@@ -15,7 +15,9 @@ def sample(src: SparseTensor, num_neighbors: int,
         rowptr = rowptr[subset]
 
     rand = torch.rand((rowcount.size(0), num_neighbors), device=col.device)
-    rand = rand.mul_(rowcount.to(rand.dtype)).to(torch.long).add_(rowptr)
+    rand.mul_(rowcount.to(rand.dtype).view(-1, 1))
+    rand = rand.to(torch.long)
+    rand.add_(rowptr.view(-1, 1))
 
     return col[rand]
 
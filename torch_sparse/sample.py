a72001c5cfa13cd7ef101e960c25b6dba7e1a086
rusty1s <matthias.fey@tu-dortmund.de>
2020-05-18 13:12:44 +0200

    fix broadcasting

--- torch_sparse/sample.py
+++ torch_sparse/sample.py
@@ -15,7 +15,9 @@ def sample(src: SparseTensor, num_neighbors: int,
         rowptr = rowptr[subset]
 
     rand = torch.rand((rowcount.size(0), num_neighbors), device=col.device)
-    rand = rand.mul_(rowcount.to(rand.dtype)).to(torch.long).add_(rowptr)
+    rand.mul_(rowcount.to(rand.dtype).view(-1, 1))
+    rand = rand.to(torch.long)
+    rand.add_(rowptr.view(-1, 1))
 
     return col[rand]
 
