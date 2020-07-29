abf5f005ae4b10f155e009b45e97f58fa99811d4
rusty1s <matthias.fey@tu-dortmund.de>
2020-07-29 09:37:51 +0200

    test install

--- script/install.sh
+++ script/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [ "${TRAVIS_OS_NAME}" != "windows" ]; then
-  python setup.py install
-else
-  echo "pip install .[test]"
-  while sleep 540; do echo "=====[ Still running after $SECONDS seconds ]====="; done &
-  pip install .\[test\] | tail -n 1000
-  kill %1
-fi
+# if [ "${TRAVIS_OS_NAME}" != "windows" ]; then
+#   echo "LOL"
+#   pip install .\[test\]
+# else
+echo "pip install .[test]"
+while sleep 10; do echo "=====[ Still running after $SECONDS seconds ]====="; done &
+pip install .\[test\] | tail -n 1000
+kill %1
+# fi
