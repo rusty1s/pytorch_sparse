96aad4af0a8fa978dde23eb04902a449995ae14f
rusty1s <matthias.fey@tu-dortmund.de>
2020-08-05 07:47:33 +0200

    update

--- script/install.sh
+++ script/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ "${TRAVIS_OS_NAME}" != "windows" ]; then
-  pip install .\[test\]
+  python setup.py develop
 else
   echo "pip install .[test]"
   while sleep 30; do echo "=====[ Still running after $SECONDS seconds ]====="; done &
-  pip install .\[test\] | tail -n 1000
+  python setup.py develop | tail -n 1000
   kill %1
 fi
