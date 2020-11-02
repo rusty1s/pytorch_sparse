45ae8aab8f1f5c306a6eb55633a1f0f01250bcbf
rusty1s <matthias.fey@tu-dortmund.de>
2020-11-02 08:00:29 +0100

    cuda 110 builds

--- torch_sparse/__init__.py
+++ torch_sparse/__init__.py
@@ -3,7 +3,7 @@ import os.path as osp
 
 import torch
 
-__version__ = '0.6.7'
+__version__ = '0.6.8'
 
 for library in [
         '_version', '_convert', '_diag', '_spmm', '_spspmm', '_metis', '_rw',
