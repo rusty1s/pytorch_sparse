947e03699df79020b9df712ed880c9b69ed3d8e7
rusty1s <matthias.fey@tu-dortmund.de>
2021-01-11 12:41:09 +0000

    torch.half support for spmm (CPU)

--- test/test_matmul.py
+++ test/test_matmul.py
@@ -45,6 +45,20 @@ def test_spmm(dtype, device, reduce):
     assert torch.allclose(expected_grad_other, other.grad, atol=1e-6)
 
 
+def test_spmm_half_precision():
+    src_dense = torch.randn((10, 8), dtype=torch.half, device='cpu')
+    src_dense[2:4, :] = 0  # Remove multiple rows.
+    src_dense[:, 2:4] = 0  # Remove multiple columns.
+    src = SparseTensor.from_dense(src_dense)
+
+    other = torch.randn((2, 8, 2), dtype=torch.half, device='cpu')
+
+    expected = src_dense @ other
+    out = src @ other
+
+    assert torch.allclose(expected, out, atol=1e-6)
+
+
 @pytest.mark.parametrize('dtype,device', product(grad_dtypes, devices))
 def test_spspmm(dtype, device):
     src = torch.tensor([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=dtype,
