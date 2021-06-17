f63df098aa8f744ca0fa9e57dc2cff22dea8f883
rusty1s <matthias.fey@tu-dortmund.de>
2021-06-17 08:33:48 +0200

    build metis

--- .github/workflows/metis-Windows.sh
+++ .github/workflows/metis-Windows.sh
@@ -2,10 +2,10 @@
 
 METIS=metis-5.1.0
 
-wget -nv http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/${METIS}.tar.gz
-tar -xvzf ${METIS}.tar.gz
-rm -f ${METIS}.tar.gz
-cd ${METIS} || exit
+curl -k -L "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/${METIS}.tar.gz" --output "${METIS}.tar.gz"
+tar -xvzf "${METIS}.tar.gz"
+rm -f "${METIS}.tar.gz"
+cd "${METIS}" || exit
 sed -i.bak -e 's/IDXTYPEWIDTH 32/IDXTYPEWIDTH 64/g' include/metis.h
 
 # Fix GKlib on Windows: https://github.com/jlblancoc/suitesparse-metis-for-windows/issues/6
@@ -18,4 +18,4 @@ cmake --build . --config "Release" --target ALL_BUILD
 cp libmetis/Release/metis.lib /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/VC/Tools/MSVC/14.16.27023/lib/x64
 cp ../include/metis.h /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/VC/Tools/MSVC/14.16.27023/include
 
-rm -f ${METIS}
+rm -f "${METIS}"
--- .github/workflows/metis.sh
+++ .github/workflows/metis.sh
@@ -2,14 +2,14 @@
 
 METIS=metis-5.1.0
 
-wget -nv http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/${METIS}.tar.gz
-tar -xvzf ${METIS}.tar.gz
-rm -f ${METIS}.tar.gz
-cd ${METIS} || exit
+wget -nv "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/${METIS}.tar.gz"
+tar -xvzf "${METIS}.tar.gz"
+rm -f "${METIS}.tar.gz"
+cd "${METIS}" || exit
 sed -i.bak -e 's/IDXTYPEWIDTH 32/IDXTYPEWIDTH 64/g' include/metis.h
 
 make config
 make
 sudo make install
 
-rm -f ${METIS}
+rm -f "${METIS}"
