663fbf80b4e97a266fad7a9c3ec3c36807f72b92
rusty1s <matthias.fey@tu-dortmund.de>
2021-06-17 08:29:35 +0200

    build metis

---
+++ .github/workflows/metis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+METIS=metis-5.1.0
+
+wget -nv http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/${METIS}.tar.gz
+tar -xvzf ${METIS}.tar.gz
+rm -f ${METIS}.tar.gz
+cd ${METIS} || exit
+sed -i.bak -e 's/IDXTYPEWIDTH 32/IDXTYPEWIDTH 64/g' include/metis.h
+
+make config
+make
+sudo make install
+
+rm -f ${METIS}
