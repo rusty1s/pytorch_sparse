f342db64e4b73610a52ceff6b5344d7ac6a8c96d
rusty1s <matthias.fey@tu-dortmund.de>
2021-06-26 15:57:36 +0200

    [ci skip]

--- conda/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# pip install .
-python setup.py install
--- conda/pytorch-sparse/bld.bat
+++ conda/pytorch-sparse/bld.bat
@@ -1,5 +1,7 @@
 copy "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\VC\\Tools\\MSVC\\14.29.30037\\lib\\x64\\metis.lib" %LIBRARY_LIB%
+if errorlevel 1 exit 1
 copy "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\VC\\Tools\\MSVC\\14.29.30037\\include\\metis.h" %LIBRARY_INC%
+if errorlevel 1 exit 1
 
-:: pip install .
 python setup.py install
+if errorlevel 1 exit 1
