bdd1ced88d5c6e52c5cd8f5ec06cef2262b21435
rusty1s <matthias.fey@tu-dortmund.de>
2021-08-23 09:49:16 +0200

    fix MSVC

--- .github/workflows/metis-Windows.sh
+++ .github/workflows/metis-Windows.sh
@@ -2,20 +2,6 @@
 
 METIS=metis-5.1.0
 
-ls /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/
-echo "----------------"
-ls /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019
-echo "----------------"
-ls /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise
-echo "----------------"
-ls /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC
-echo "----------------"
-ls /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/Tools
-echo "----------------"
-ls /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/Tools/MSVC
-echo "----------------"
-ls /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30037
-
 curl -k -L "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/${METIS}.tar.gz" --output "${METIS}.tar.gz"
 tar -xvzf "${METIS}.tar.gz"
 rm -f "${METIS}.tar.gz"
@@ -29,7 +15,7 @@ cd build || exit
 
 cmake .. -A x64  # Ensure we are building with x64
 cmake --build . --config "Release" --target ALL_BUILD
-cp libmetis/Release/metis.lib /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30037/lib/x64
-cp ../include/metis.h /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30037/include
+cp libmetis/Release/metis.lib /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/lib/x64
+cp ../include/metis.h /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/include
 
 rm -f "${METIS}"
