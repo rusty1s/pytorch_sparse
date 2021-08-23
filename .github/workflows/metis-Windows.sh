1008c2153fbbb87b28316900899b2675ce6b74ea
rusty1s <matthias.fey@tu-dortmund.de>
2021-08-23 09:23:13 +0200

    fix MSVC

--- .github/workflows/metis-Windows.sh
+++ .github/workflows/metis-Windows.sh
@@ -2,6 +2,20 @@
 
 METIS=metis-5.1.0
 
+ls /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/
+echo "----------------"
+ls /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019
+echo "----------------"
+ls /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise
+echo "----------------"
+ls /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC
+echo "----------------"
+ls /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/Tools
+echo "----------------"
+ls /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/Tools/MSVC
+echo "----------------"
+ls /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30037
+
 curl -k -L "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/${METIS}.tar.gz" --output "${METIS}.tar.gz"
 tar -xvzf "${METIS}.tar.gz"
 rm -f "${METIS}.tar.gz"
