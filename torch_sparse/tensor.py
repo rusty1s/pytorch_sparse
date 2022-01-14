109d23ba5f055af443c4a8d5003b1112aa3eb545
rusty1s <matthias.fey@tu-dortmund.de>
2022-01-14 17:27:11 +0100

    typo

--- torch_sparse/tensor.py
+++ torch_sparse/tensor.py
@@ -491,7 +491,10 @@ def cuda(self, device: Optional[Union[int, str]] = None,
 def __getitem__(self: SparseTensor, index: Any) -> SparseTensor:
     index = list(index) if isinstance(index, tuple) else [index]
     # More than one `Ellipsis` is not allowed...
-    if len([i for i in index if i == ...]) > 1:
+    if len([
+            i for i in index
+            if not isinstance(i, (torch.Tensor, np.ndarray)) and i == ...
+    ]) > 1:
         raise SyntaxError
 
     dim = 0
