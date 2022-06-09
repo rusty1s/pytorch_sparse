514a43771b72402ffa7be97769b916107e74bb59
berkekisin <kisinberke@gmail.com>
2022-06-10 01:56:19 +0200

    add value support, general interface and documentation

--- csrc/cuda/spmm_coo_cuda.cu
+++ csrc/cuda/spmm_coo_cuda.cu
@@ -21,33 +21,12 @@ inline cudaError_t checkCuda(cudaError_t result)
   return result;
 }
 
-template <typename scalar_t, ReductionType REDUCE>
+template <typename scalar_t, ReductionType REDUCE, bool HAS_VALUE>
 __global__ void spmm_coo_kernel(
     const scalar_t* __restrict__ mat,
     const int64_t* __restrict__ row, 
     const int64_t* __restrict__ col,
-    scalar_t* __restrict__ res,
-    size_t hidden_dim,
-    size_t N)
-{
-    
-    int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
-    
-    if(thread_id < N){
-        int edge_index = thread_id / hidden_dim;
-        int hidden_dim_index = thread_id % hidden_dim;      
-
-        Reducer<scalar_t, REDUCE>::atomic_write(
-           res + col[edge_index]*hidden_dim + hidden_dim_index, 
-            mat[ row[edge_index]*hidden_dim + hidden_dim_index]);  
-    }
-}
-
-template <typename scalar_t>
-__global__ void spmm_coo_arg_kernel(
-    const scalar_t* __restrict__ mat,
-    const int64_t* __restrict__ row, 
-    const int64_t* __restrict__ col,
+    const scalar_t* __restrict__ value,
     scalar_t* __restrict__ res,
     int64_t* __restrict__ arg_out,
     size_t hidden_dim,
@@ -55,20 +34,40 @@ __global__ void spmm_coo_arg_kernel(
 {
     
     int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
-
+    
     if(thread_id < N){
         int edge_index = thread_id / hidden_dim;
         int hidden_dim_index = thread_id % hidden_dim;
 
-        if(res[col[edge_index]*hidden_dim + hidden_dim_index] == mat[row[edge_index]*hidden_dim + hidden_dim_index]){
-            arg_out[col[edge_index]*hidden_dim + hidden_dim_index] = row[edge_index];
+        int edge_start = __ldg(row + edge_index);
+        int edge_end = __ldg(col + edge_index);
+        scalar_t write_val = __ldg(mat + edge_start*hidden_dim + hidden_dim_index);
+        int res_index = edge_end*hidden_dim + hidden_dim_index;
+        
+        if(HAS_VALUE)
+            write_val *= __ldg(value + edge_index);
+          
+        Reducer<scalar_t, REDUCE>::atomic_write(
+                res + res_index, 
+                write_val);
+        
+        //compute arg out tensor
+        if(REDUCE == MIN || REDUCE == MAX){
+            __syncthreads();
+            if (HAS_VALUE)
+               edge_start = edge_index;
+
+            if(res[res_index] == write_val){
+                arg_out[res_index] = edge_start;
+            }
         }
     }
 }
 
 std::tuple<torch::Tensor,torch::optional<torch::Tensor>> spmm_coo_cuda(
     const torch::Tensor row, 
-    const torch::Tensor col, 
+    const torch::Tensor col,
+    const torch::optional<torch::Tensor> optional_value, 
     torch::Tensor mat,
     int64_t dim_size,
     std::string reduce)
@@ -77,7 +76,12 @@ std::tuple<torch::Tensor,torch::optional<torch::Tensor>> spmm_coo_cuda(
     CHECK_CUDA(row);
     CHECK_CUDA(col);
     CHECK_CUDA(mat);
-    CHECK_INPUT(row.size(0) == col.size(0));
+    CHECK_INPUT(row.size(0) == col.size(0)); 
+    if (optional_value.has_value()){
+        CHECK_CUDA(optional_value.value());
+        CHECK_INPUT(optional_value.value().dim() == 1);
+        CHECK_INPUT(optional_value.value().size(0) == col.size(0));
+    }
     mat = mat.contiguous();
     
     // variables for loops
@@ -93,9 +97,9 @@ std::tuple<torch::Tensor,torch::optional<torch::Tensor>> spmm_coo_cuda(
     torch::optional<torch::Tensor> arg_out = torch::nullopt;
     int64_t *arg_out_data = nullptr;
     if (reduce2REDUCE.at(reduce) == MIN || reduce2REDUCE.at(reduce) == MAX) {
-        arg_out = torch::full_like(res,mat.size(0),row.options());
+        arg_out = torch::full_like(res,col.size(0),row.options());
         arg_out_data = arg_out.value().data_ptr<int64_t>();
-      }
+    }
 
     auto row_data = row.data_ptr<int64_t>();
     auto col_data = col.data_ptr<int64_t>();
@@ -108,25 +112,30 @@ std::tuple<torch::Tensor,torch::optional<torch::Tensor>> spmm_coo_cuda(
         AT_DISPATCH_REDUCTION_TYPES(reduce, [&] {
             res.fill_(Reducer<scalar_t, REDUCE>::init());
 
-            spmm_coo_kernel<scalar_t, REDUCE><<<BLOCKS(N), THREADS>>>(
-                mat_data,
-                row_data,
-                col_data,
-                res_data,
-                hidden_dim,
-                N);
-    
-            res.masked_fill_(res == Reducer<scalar_t, REDUCE>::init(), (scalar_t)0);
-            if (REDUCE == MIN || REDUCE == MAX){
-                spmm_coo_arg_kernel<scalar_t><<<BLOCKS(N), THREADS>>>(
+            if(optional_value.has_value()){
+                auto value_data = optional_value.value().data_ptr<scalar_t>();
+                spmm_coo_kernel<scalar_t, REDUCE, true><<<BLOCKS(N), THREADS>>>(
                     mat_data,
                     row_data,
                     col_data,
+                    value_data,
                     res_data,
                     arg_out_data,
                     hidden_dim,
-                    N);
-            }      
+                    N);     
+            }else{
+                spmm_coo_kernel<scalar_t, REDUCE, false><<<BLOCKS(N), THREADS>>>(
+                    mat_data,
+                    row_data,
+                    col_data,
+                    nullptr,
+                    res_data,
+                    arg_out_data,
+                    hidden_dim,
+                    N);     
+            }
+
+            res.masked_fill_(res == Reducer<scalar_t, REDUCE>::init(), (scalar_t)0);
         });     
     });
 
--- csrc/spmm_coo.cpp
+++ csrc/spmm_coo.cpp
@@ -27,49 +27,66 @@ torch::Tensor broadcast(torch::Tensor src, torch::Tensor other, int64_t dim)
 std::tuple<torch::Tensor, torch::optional<torch::Tensor>> spmm_coo_fw(
     const torch::Tensor row,
     const torch::Tensor col,
+    const torch::optional<torch::Tensor> optional_value,
     const torch::Tensor mat,
     int64_t dim_size,
     std::string reduce)
 {
-    if(row.device().is_cuda()){
-        return spmm_coo_cuda(row, col, mat, dim_size, reduce);
+    if (row.device().is_cuda())
+    {
+        return spmm_coo_cuda(row, col, optional_value, mat, dim_size, reduce);
     }
-    else{
+    else
+    {
         return spmm_coo_cpu(row, col, mat, dim_size, reduce);
     }
-        //AT_ERROR("Row Tensor not in GPU!");
+    // AT_ERROR("Row Tensor not in GPU!");
 }
 
 using torch::autograd::AutogradContext;
 using torch::autograd::Variable;
 using torch::autograd::variable_list;
 
-
 class SPMMSum : public torch::autograd::Function<SPMMSum>
 {
 public:
     static variable_list forward(AutogradContext *ctx, Variable row,
                                  Variable col,
+                                 Variable value,
                                  Variable mat,
-                                 int64_t dim_size)
+                                 int64_t dim_size,
+                                 bool has_value)
     {
         ctx->saved_data["mat_shape"] = mat.sizes();
-        auto result = spmm_coo_fw(row, col, mat, dim_size, "sum");
-        auto out = std::get<0>(result);
-        ctx->save_for_backward({row, col});
-        ctx->mark_non_differentiable({row, col});
+        ctx->saved_data["has_value"] = has_value;
+        torch::optional<torch::Tensor> opt_value = torch::nullopt;
+        if (has_value)
+            opt_value = value;
+
+        auto out = std::get<0>(spmm_coo_fw(row, col, opt_value, mat, dim_size, "sum"));
+        ctx->save_for_backward({row, col, value});
+        ctx->mark_non_differentiable({row, col, value});
+
         return {out};
     }
 
     static variable_list backward(AutogradContext *ctx, variable_list grad_outs)
     {
-        auto grad_out = grad_outs[0];
-        auto row = ctx->get_saved_variables()[0];
-        auto col = ctx->get_saved_variables()[1];
+        auto has_value = ctx->saved_data["has_value"].toBool();
         auto mat_shape = list2vec(ctx->saved_data["mat_shape"].toIntList());
-        auto result = spmm_coo_fw(col, row, grad_out, mat_shape[0], "sum");
-        auto grad_in = std::get<0>(result);
-        return {Variable(),Variable(),grad_in, Variable()};
+        auto grad_out = grad_outs[0];
+        auto saved = ctx->get_saved_variables();
+        auto row = saved[0];
+        auto col = saved[1];
+        auto value = saved[2];
+
+        // compute gradient of mat
+        torch::optional<torch::Tensor> opt_value = torch::nullopt;
+        if (has_value)
+            opt_value = value;
+        auto grad_mat = std::get<0>(spmm_coo_fw(col, row, opt_value, grad_out, mat_shape[0], "sum"));
+
+        return {Variable(), Variable(), Variable(), grad_mat, Variable(), Variable()};
     }
 };
 
@@ -78,40 +95,53 @@ class SPMMMean : public torch::autograd::Function<SPMMMean>
 public:
     static variable_list forward(AutogradContext *ctx, Variable row,
                                  Variable col,
+                                 Variable value,
                                  Variable mat,
-                                 int64_t dim_size)
+                                 int64_t dim_size,
+                                 bool has_value)
     {
         ctx->saved_data["mat_shape"] = mat.sizes();
-        auto result = spmm_coo_fw(row, col, mat, dim_size, "sum");
-        auto out = std::get<0>(result);
+        ctx->saved_data["has_value"] = has_value;
+        torch::optional<torch::Tensor> opt_value = torch::nullopt;
+        if (has_value)
+            opt_value = value;
+        auto out = std::get<0>(spmm_coo_fw(row, col, opt_value, mat, dim_size, "sum"));
+
         // compute degree of elements in result tensor
-        auto ones = torch::ones(size(mat,0), mat.options());
-        result = spmm_coo_fw(row, col, ones, dim_size, "sum");
-        auto degree = std::get<0>(result);
+        auto ones = torch::ones(size(mat, 0), mat.options());
+        auto degree = std::get<0>(spmm_coo_fw(row, col, torch::nullopt, ones, dim_size, "sum"));
         degree.masked_fill_(degree < 1, 1);
+
         // divide result tensor by degree
         degree = broadcast(degree, out, 0);
         if (out.is_floating_point())
             out.true_divide_(degree);
         else
             out.div_(degree, "floor");
-        ctx->save_for_backward({row, col, degree});
-        ctx->mark_non_differentiable({row, col, degree});
+        ctx->save_for_backward({row, col, degree, value});
+        ctx->mark_non_differentiable({row, col, degree, value});
+
         return {out};
     }
 
     static variable_list backward(AutogradContext *ctx, variable_list grad_outs)
     {
+        auto has_value = ctx->saved_data["has_value"].toBool();
+        auto mat_shape = list2vec(ctx->saved_data["mat_shape"].toIntList());
         auto grad_out = grad_outs[0].clone();
         auto saved = ctx->get_saved_variables();
         auto row = saved[0];
         auto col = saved[1];
         auto degree = saved[2];
-        auto mat_shape = list2vec(ctx->saved_data["mat_shape"].toIntList());
+        auto value = saved[3];
+
         grad_out.true_divide_(degree);
-        auto result = spmm_coo_fw(col, row, grad_out, mat_shape[0], "sum");
-        auto grad_in = std::get<0>(result);
-        return { Variable(), Variable(),grad_in, Variable()};
+        torch::optional<torch::Tensor> opt_value = torch::nullopt;
+        if (has_value)
+            opt_value = value;
+        auto grad_mat = std::get<0>(spmm_coo_fw(col, row, opt_value, grad_out, mat_shape[0], "sum"));
+
+        return {Variable(), Variable(), Variable(), grad_mat, Variable(), Variable()};
     }
 };
 
@@ -120,68 +150,92 @@ class SPMMMax : public torch::autograd::Function<SPMMMax>
 public:
     static variable_list forward(AutogradContext *ctx, Variable row,
                                  Variable col,
+                                 Variable value,
                                  Variable mat,
-                                 int64_t dim_size)
+                                 int64_t dim_size,
+                                 bool has_value)
     {
         ctx->saved_data["mat_shape"] = mat.sizes();
-        auto result = spmm_coo_fw(row, col, mat, dim_size, "max");
+        ctx->saved_data["has_value"] = has_value;
+        torch::optional<torch::Tensor> opt_value = torch::nullopt;
+        if (has_value)
+            opt_value = value;
+
+        auto result = spmm_coo_fw(row, col, opt_value, mat, dim_size, "max");
         auto out = std::get<0>(result);
         auto arg_out = std::get<1>(result).value();
-        ctx->save_for_backward({arg_out});
-        ctx->mark_non_differentiable({arg_out});
+        ctx->save_for_backward({arg_out, value,row});
+        ctx->mark_non_differentiable({row, col, arg_out, value});
+
         return {out, arg_out};
     }
 
     static variable_list backward(AutogradContext *ctx, variable_list grad_outs)
     {
-        auto grad_out = grad_outs[0];
-        auto arg_out = ctx->get_saved_variables()[0];
+        auto has_value = ctx->saved_data["has_value"].toBool();
         auto mat_shape = list2vec(ctx->saved_data["mat_shape"].toIntList());
-        mat_shape[0] += 1;
-        auto grad_in = torch::zeros(mat_shape, grad_out.options());
-        grad_in.scatter_(0, arg_out, grad_out, "add");
-        grad_in = grad_in.narrow(0, 0, mat_shape[0] - 1);
-        return {Variable(), Variable(), grad_in, Variable()};
+        auto grad_out = grad_outs[0];
+        auto saved = ctx->get_saved_variables();
+        auto arg_out = saved[0];
+        auto value = saved[1];
+        auto row = saved[2];
+        //mat_shape[0] += 1;
+        auto invalid_arg_mask = arg_out == row.size(0);
+        arg_out = arg_out.masked_fill(invalid_arg_mask, 0);
+        grad_out = grad_out.masked_fill(invalid_arg_mask, 0);
+
+        //compute gradient of mat
+        auto grad_mat = torch::zeros(mat_shape, grad_out.options());
+        if (has_value){
+            grad_out = value.index_select(0, arg_out.flatten()).view_as(grad_out).mul_(grad_out);
+            arg_out = row.index_select(0,arg_out.flatten()).view_as(grad_out);
+        }
+        grad_mat.scatter_(0, arg_out, grad_out,"add");
+
+        return {Variable(), Variable(),Variable(), grad_mat, Variable(),Variable()};
     }
 };
 
-SPARSE_API std::tuple<torch::Tensor, torch::Tensor> spmm_coo_max(const torch::Tensor row,
-                                                      const torch::Tensor col,
-                                                      const torch::Tensor mat,
-                                                      int64_t dim_size)
+SPARSE_API torch::Tensor spmm_coo_sum(const torch::Tensor row,
+                                      const torch::Tensor col,
+                                      const torch::optional<torch::Tensor> opt_value,
+                                      const torch::Tensor mat,
+                                      int64_t dim_size)
 {
-    auto result = SPMMMax::apply(row, col, mat, dim_size);
+    auto value = opt_value.has_value() ? opt_value.value() : col;
+    return SPMMSum::apply(row, col, value, mat, dim_size, opt_value.has_value())[0];
+}
+
+SPARSE_API std::tuple<torch::Tensor, torch::Tensor> spmm_coo_max(const torch::Tensor row,
+                                                                 const torch::Tensor col,
+                                                                 const torch::optional<torch::Tensor> opt_value,
+                                                                 const torch::Tensor mat,
+                                                                 int64_t dim_size)
+{
+    auto value = opt_value.has_value() ? opt_value.value() : col;
+    auto result = SPMMMax::apply(row, col, value, mat, dim_size, opt_value.has_value());
 
     return std::make_tuple(result[0], result[1]);
 }
 
-SPARSE_API torch::Tensor spmm_coo_sum(const torch::Tensor row,
-                           const torch::Tensor col,
-                           const torch::Tensor mat,
-                           int64_t dim_size)
-{
-
-    return SPMMSum::apply( row, col, mat, dim_size)[0];
-}
-
 SPARSE_API torch::Tensor spmm_coo_mean(const torch::Tensor row,
-                            const torch::Tensor col,
-                            const torch::Tensor mat,
-                            int64_t dim_size)
+                                       const torch::Tensor col,
+                                       const torch::optional<torch::Tensor> opt_value,
+                                       const torch::Tensor mat,
+                                       int64_t dim_size)
 {
-
-    return SPMMMean::apply(row, col, mat, dim_size)[0];
+    auto value = opt_value.has_value() ? opt_value.value() : col;
+    return SPMMMean::apply(row, col, value, mat, dim_size, opt_value.has_value())[0];
 }
-
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
-  m.def("spmm_coo_sum", &spmm_coo_sum, "Sum Sparse Mul forward");
-  m.def("spmm_coo_max", &spmm_coo_max, "Max Sparse Mul forward");
-  m.def("spmm_coo_mean", &spmm_coo_mean, "Mean Sparse Mul forward");
+    m.def("spmm_coo_sum", &spmm_coo_sum, "Sum Sparse Mul forward");
+    m.def("spmm_coo_max", &spmm_coo_max, "Max Sparse Mul forward");
+    m.def("spmm_coo_mean", &spmm_coo_mean, "Mean Sparse Mul forward");
 }
 
 static auto registry = torch::RegisterOperators()
-                           .op("torch_sparse::spmm_coo_sum", &spmm_coo_sum)
-                           .op("torch_sparse::spmm_coo_mean", &spmm_coo_mean)
-                           .op("torch_sparse::spmm_coo_max", &spmm_coo_max);
+                           .op("torch_sparse::spmm_coo_sum", &spmm_coo_sum);
+//.op("torch_sparse::spmm_coo_mean", &spmm_coo_mean)
+//.op("torch_sparse::spmm_coo_max", &spmm_coo_max);
