587d1d02b2461665e4902aa5cd7071f59c22b680
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-06-30 13:25:32 +0200

    Version up (#246)
    * PyTorch 1.12 support
    
    * version up

--- setup.py
+++ setup.py
@@ -8,10 +8,14 @@ from itertools import product
 import torch
 from setuptools import find_packages, setup
 from torch.__config__ import parallel_info
-from torch.utils.cpp_extension import (CUDA_HOME, BuildExtension, CppExtension,
-                                       CUDAExtension)
+from torch.utils.cpp_extension import (
+    CUDA_HOME,
+    BuildExtension,
+    CppExtension,
+    CUDAExtension,
+)
 
-__version__ = '0.7.0'
+__version__ = '0.6.14'
 URL = 'https://github.com/rusty1s/pytorch_sparse'
 
 WITH_CUDA = torch.cuda.is_available() and CUDA_HOME is not None
