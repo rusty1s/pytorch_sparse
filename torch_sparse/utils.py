a980efd4fd9f7d87dbcf440759c4a875bf3eb600
Damian Szwichtenberg <damian.szwichtenberg@intel.com>
2023-02-01 20:39:47 +0100

    Enable `index_sort` (#306)
    * Enable `index_sort`
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: rusty1s <matthias.fey@tu-dortmund.de>

--- torch_sparse/utils.py
+++ torch_sparse/utils.py
@@ -1,9 +1,24 @@
-from typing import Any
+from typing import Any, Optional, Tuple
+
+import torch
+
+import torch_sparse.typing
+from torch_sparse.typing import pyg_lib
 
 try:
     from typing_extensions import Final  # noqa
 except ImportError:
     from torch.jit import Final  # noqa
+
+
+def index_sort(
+        inputs: torch.Tensor,
+        max_value: Optional[int] = None) -> Tuple[torch.Tensor, torch.Tensor]:
+    r"""See pyg-lib documentation for more details:
+    https://pyg-lib.readthedocs.io/en/latest/modules/ops.html"""
+    if not torch_sparse.typing.WITH_INDEX_SORT:  # pragma: no cover
+        return inputs.sort()
+    return pyg_lib.ops.index_sort(inputs, max_value)
 
 
 def is_scalar(other: Any) -> bool:
