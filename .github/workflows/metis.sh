05b62b1c02447f28f87dfe2d44ae8d8d2e0204ba
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-11-09 16:26:22 +0100

    Support for older `GLIBC` versions (#353)
    * update
    
    * update

--- .github/workflows/metis.sh
+++ .github/workflows/metis.sh
@@ -7,6 +7,8 @@ tar -xvzf "${METIS}.tar.gz"
 rm -f "${METIS}.tar.gz"
 cd "${METIS}" || exit
 sed -i.bak -e 's/IDXTYPEWIDTH 32/IDXTYPEWIDTH 64/g' include/metis.h
+sed -i '1s/^/#if defined(__linux__) \&\& defined(__x86_64__)\n__asm__(".symver log,log@GLIBC_2.2.5");\n#endif\n/' GKlib/gk_proto.h
+sed -i '1s/^/#if defined(__linux__) \&\& defined(__x86_64__)\n__asm__(".symver pow,pow@GLIBC_2.2.5");\n#endif\n/' libmetis/metislib.h
 
 make config
 make
