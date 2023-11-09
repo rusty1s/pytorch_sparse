f3fb726735d96921e6e90f9bd83ca8e85cc6968b
rusty1s <matthias.fey@tu-dortmund.de>
2023-11-09 15:02:36 +0000

    update

--- .github/workflows/metis.sh
+++ .github/workflows/metis.sh
@@ -2,11 +2,13 @@
 
 METIS=metis-5.1.0
 
-wget -nv "https://web.archive.org/web/20211119110155/http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/${METIS}.tar.gz"
+wget -nv "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-${METIS}.tar.gz"
 tar -xvzf "${METIS}.tar.gz"
 rm -f "${METIS}.tar.gz"
 cd "${METIS}" || exit
 sed -i.bak -e 's/IDXTYPEWIDTH 32/IDXTYPEWIDTH 64/g' include/metis.h
+sed -i '1s/^/#if defined(__linux__) \&\& defined(__x86_64__)\n__asm__(".symver log,log@GLIBC_2.2.5");\n#endif\n/' GKlib/gk_proto.h
+sed -i '1s/^/#if defined(__linux__) \&\& defined(__x86_64__)\n__asm__(".symver pow,pow@GLIBC_2.2.5");\n#endif\n/' libmetis/metislib.h
 
 make config
 make
