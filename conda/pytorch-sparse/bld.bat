02fd73515ab4fe7826ad42997026fd8cc8b23753
Matthias Fey <matthias.fey@tu-dortmund.de>
2025-04-10 21:26:48 +0200

    PyTorch 2.6 support (#397)
    update

--- conda/pytorch-sparse/bld.bat
+++
@@ -1,7 +0,0 @@
-copy "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\VC\\Tools\\MSVC\\14.29.30133\\lib\\x64\\metis.lib" %LIBRARY_LIB%
-if errorlevel 1 exit 1
-copy "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\VC\\Tools\\MSVC\\14.29.30133\\include\\metis.h" %LIBRARY_INC%
-if errorlevel 1 exit 1
-
-"%PYTHON%" -m pip install .
-if errorlevel 1 exit 1
