06bcbf53a863617bd9527eb20b67614dd3580d52
rusty1s <matthias.fey@tu-dortmund.de>
2026-03-09 12:27:57 +0000

    update

--- .github/workflows/metis.sh
+++ .github/workflows/metis.sh
@@ -9,6 +9,7 @@ cd "${METIS}" || exit
 sed -i.bak -e 's/IDXTYPEWIDTH 32/IDXTYPEWIDTH 64/g' include/metis.h
 sed -i '1s/^/#if defined(__linux__) \&\& defined(__x86_64__)\n__asm__(".symver log,log@GLIBC_2.2.5");\n#endif\n/' GKlib/gk_proto.h
 sed -i '1s/^/#if defined(__linux__) \&\& defined(__x86_64__)\n__asm__(".symver pow,pow@GLIBC_2.2.5");\n#endif\n/' libmetis/metislib.h
+sed -i.bak 's/cmake_minimum_required(VERSION 2\.8)/cmake_minimum_required(VERSION 3.5)/' CMakeLists.txt GKlib/CMakeLists.txt
 
 make config
 make
